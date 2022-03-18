d70328f7281c75c4e3eea88e18d9952f85f5e92e
Nico vega <46005722+NicoVegaOfficial@users.noreply.github.com>
2022-03-18 02:33:05 -0300

    Se añade timestamp

--- BlockChain/Program.cs
+++ BlockChain/Program.cs
@@ -8,6 +8,7 @@ namespace BlockChain
     {
         public int index { get; set; }
         public string prevHash { get; set; }
+        public long Timestamp { get; set; }
         public string data { get; set; }
     }
     public class genesisBlock
@@ -18,6 +19,7 @@ namespace BlockChain
             {
                 index = 0,
                 prevHash = "891e2f58bc689b757413684ad49e53b4cd967d245c2594586548bffe20f48324a6d95634a45afde174a2e297bd20807a5d2e96d7724760ea2fb96220ac3b270f",
+                Timestamp = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds(),
                 data = "Datos"
             };
             var options = new JsonSerializerOptions { WriteIndented = true };
@@ -55,9 +57,7 @@ namespace BlockChain
         {
             genesisBlock nBlock = new genesisBlock();
             calcHash nhash = new calcHash();
-            Console.WriteLine(nhash.sha512("el dinero es el sistema operativo del mundo"));
-            //nBlock.iniciar();
+            nBlock.iniciar();
         }
-
     }
 }
