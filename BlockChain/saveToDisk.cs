e227c12fdc8058e29923fff2ee14a41e36f6edaf
Nicolás Vega <nicolasvegaguerrero10@gmail.com>
2022-07-25 19:41:21 -0300

    changes for Linux Systems

--- BlockChain/saveToDisk.cs
+++ BlockChain/saveToDisk.cs
@@ -1,29 +1,37 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace BlockChain
 {
     public class saveToDisk
     {
-
         public void save(string name, string input)
         {
-            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            string appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            string filepath = desktop + @"\folder\" + name + ".json";
+            string appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\FireBlade";
+            string file = appdata + @"\" + name + ".json";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) == true)
+            {
+                appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/FireBlade";
+                file = appdata + @"/" + name + ".json";
+            }
+            if (!Directory.Exists(appdata))
+            {
+                Directory.CreateDirectory(appdata);
+            }
             try
             {
-                if (File.Exists(filepath))
+                if (File.Exists(file))
                 {
-                    File.Delete(filepath);
+                    File.Delete(file);
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
-            using (FileStream fs = File.Create(filepath))
+            using (FileStream fs = File.Create(file))
             {
                 Byte[] info = new UTF8Encoding(true).GetBytes(input);
                 fs.Write(info, 0, info.Length);
